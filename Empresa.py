873d38bfdb1c97cb306223329b03becc8fa17d59
Guillermo Santander <guillermosantander343@gmail.com>
2024-07-17 19:31:40 -0400

    Trabajadores

--- Empresa.py
+++ Empresa.py
@@ -3,9 +3,22 @@ import random
 def generate_random_balances(num_accounts):
     return [random.randint(300.000, 2500000) for _ in range(num_accounts)]
 
-print("Bienvenido al menu de Opciones")
+trabajadores = [
+    "Jose Alvaro",
+    "Maria Isabel",
+    "Pedro Pablo",
+    "Juan Ignacio",
+    "Antonio José",
+    "Sofia Maria",
+]
+
+
+nombre = int(input("Digite su nombre: "))
+
+print("Bienvenido al menu de Opciones {nombre}")
 print("1. Asignar sueldos aleatorios")
 print("2. Clasificar sueldos")
 print("3. Ver estadísticas.")
 print("4. Reporte de sueldos")
 print("5. Salir del programa")
+
