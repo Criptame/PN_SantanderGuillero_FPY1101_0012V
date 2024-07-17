c9f4e82461625ff616f6157da48a3260a161e6df
Guillermo Santander <guillermosantander343@gmail.com>
2024-07-17 19:24:23 -0400

    Menu de opciones

--- Empresa.py
+++ Empresa.py
@@ -3,4 +3,9 @@ import random
 def generate_random_balances(num_accounts):
     return [random.randint(300.000, 2500000) for _ in range(num_accounts)]
 
-print("Bienvenido a ")
+print("Bienvenido al menu de Opciones")
+print("1. Asignar sueldos aleatorios")
+print("2. Clasificar sueldos")
+print("3. Ver estadísticas.")
+print("4. Reporte de sueldos")
+print("5. Salir del programa")
